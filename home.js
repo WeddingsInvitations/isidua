4ae158748f1366bfb5a6a06a4dde7f9984563af6
WeddingsInvitations <anangrifky31@gmail.com>
2025-05-02 22:58:44 +0700

    Update home.js

--- home.js
+++ home.js
@@ -1,11 +1,9 @@
-<script>
-  window.addEventListener('scroll', () => {
-    const photo = document.querySelector('.couple-photo');
-    const trigger = window.innerHeight * 0.8;
+window.addEventListener('scroll', () => {
+  const photo = document.querySelector('.couple-photo');
+  const trigger = window.innerHeight * 0.8;
 
-    if (photo.getBoundingClientRect().top < trigger) {
-      photo.style.opacity = '1';
-      photo.style.transform = 'translateY(0)';
-    }
-  });
-</script>
+  if (photo.getBoundingClientRect().top < trigger) {
+    photo.style.opacity = '1';
+    photo.style.transform = 'translateY(0)';
+  }
+});
