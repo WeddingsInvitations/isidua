020a25bec77602c6af87b05b93d2b8b93d46ce85
WeddingsInvitations <anangrifky31@gmail.com>
2025-05-02 23:22:50 +0700

    Update home.js

--- home.js
+++ home.js
@@ -3,7 +3,6 @@ window.addEventListener('scroll', () => {
   const trigger = window.innerHeight * 0.8;
 
   if (photo.getBoundingClientRect().top < trigger) {
-    photo.style.opacity = '1';
-    photo.style.transform = 'translateY(0)';
+    photo.classList.add('visible');
   }
 });
